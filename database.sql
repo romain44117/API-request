e767bbc319f902e91304a970077d9c137aea6812
romain <romain.legal44@gmail.com>
2019-07-12 16:27:36 +0200

    update database.sql

--- database.sql
+++ database.sql
@@ -14,3 +14,10 @@ CREATE TABLE IF NOT EXISTS `request` (
 
   `budget` int(11));
 
+INSERT INTO `request` (title, description, created_at, budget) VALUES (jardinage,loue tondeuse a la journée,2017-08-17 06:41:50,20);
+INSERT INTO `request` (title, description, created_at, budget) VALUES (service,garde animal de compagnie a la journéee ou semaine,2017-08-18 06:41:50,10);
+INSERT INTO `request` (title, description, created_at, budget) VALUES (sport,cherche coach sportif deux fois par semaine,2017-06-18 06:41:50,30);
+INSERT INTO `request` (title, description, created_at, budget) VALUES (bricolage,offreservice pour travaux de carrelage menuiserie et plomberie,2017-08-17 06:41:50,10);
+INSERT INTO `request` (title, description, created_at, budget) VALUES (jardinage,loue remorque ,2017-03-18 06:41:50,40);
+
+
